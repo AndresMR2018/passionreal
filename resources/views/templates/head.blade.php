38aecead9014c9644ba3eed991073aaa19c72d51
luiscoro <luiscoronel_97@hotmail.com>
2021-12-20 17:24:39 -0500

    estilos

--- resources/views/templates/head.blade.php
+++ resources/views/templates/head.blade.php
@@ -1,48 +1,44 @@
 <head>
   <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-  <!--[if IE]>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <![endif]-->
   <meta name="description" content="">
   <meta name="author" content="ScriptsBundle">
-  <title>Pasionreal</title>
+  <title>Pasión Real | @yield('title')</title>
   <!-- =-=-=-=-=-=-= Favicons Icon =-=-=-=-=-=-= -->
-  <link rel="icon" href="images/logo/logoPassionReal.jpeg" type="image/x-icon" />
+  <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
   <!-- =-=-=-=-=-=-= Mobile Specific =-=-=-=-=-=-= -->
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <!-- =-=-=-=-=-=-= Bootstrap CSS Style =-=-=-=-=-=-= -->
-  <link rel="stylesheet" href="{{asset(" css/bootstrap.css")}}">
+  <link rel="stylesheet" href="{{asset("css/bootstrap.css")}}">
   <!-- =-=-=-=-=-=-= Template CSS Style =-=-=-=-=-=-= -->
-  <link rel="stylesheet" href="{{asset(" css/style.css")}}">
+  <link rel="stylesheet" href="{{asset("css/style.css")}}">
   <!-- =-=-=-=-=-=-= Font Awesome =-=-=-=-=-=-= -->
-  <link rel="stylesheet" href="{{asset(" css/font-awesome.css")}}" type="text/css">
+  <link rel="stylesheet" href="{{asset("css/font-awesome.css")}}" type="text/css">
   <!-- =-=-=-=-=-=-= Flat Icon =-=-=-=-=-=-= -->
-  <link href="{{asset('css/flaticon.css')}}" rel="stylesheet">
+  <link href="css/flaticon.css" rel="stylesheet">
   <!-- =-=-=-=-=-=-= Et Line Fonts =-=-=-=-=-=-= -->
-  <link rel="stylesheet" href="{{asset(" css/et-line-fonts.css")}}" type="text/css">
+  <link rel="stylesheet" href="{{asset("css/et-line-fonts.css")}}" type="text/css">
   <!-- =-=-=-=-=-=-= Menu Drop Down =-=-=-=-=-=-= -->
-  <link rel="stylesheet" href="{{asset(" css/forest-menu.css")}}" type="text/css">
+  <link rel="stylesheet" href="{{asset("css/forest-menu.css")}}" type="text/css">
   <!-- =-=-=-=-=-=-= Animation =-=-=-=-=-=-= -->
-  <link rel="stylesheet" href="{{asset(" css/animate.min.css")}}" type="text/css">
+  <link rel="stylesheet" href="{{asset("css/animate.min.css")}}" type="text/css">
   <!-- =-=-=-=-=-=-= Select Options =-=-=-=-=-=-= -->
-  <link href="{{asset(" css/select2.min.css")}}" rel="stylesheet" />
+  <link href="{{asset("css/select2.min.css")}}" rel="stylesheet" />
   <!-- =-=-=-=-=-=-= noUiSlider =-=-=-=-=-=-= -->
-  <link href="{{asset(" css/nouislider.min.css")}}" rel="stylesheet">
+  <link href="{{asset("css/nouislider.min.css")}}" rel="stylesheet">
   <!-- =-=-=-=-=-=-= Listing Slider =-=-=-=-=-=-= -->
-  <link href="{{asset(" css/slider.css")}}" rel="stylesheet">
+  <link href="{{asset("css/slider.css")}}" rel="stylesheet">
   <!-- =-=-=-=-=-=-= Owl carousel =-=-=-=-=-=-= -->
-  <link rel="stylesheet" type="text/css" href="{{asset(" css/owl.carousel.css")}}">
-  <link rel="stylesheet" type="text/css" href="{{asset(" css/owl.theme.css")}}">
+  <link rel="stylesheet" type="text/css" href="{{asset("css/owl.carousel.css")}}">
+  <link rel="stylesheet" type="text/css" href="{{asset("css/owl.theme.css")}}">
   <!-- =-=-=-=-=-=-= Check boxes =-=-=-=-=-=-= -->
-  <link href="{{asset(" skins/minimal/minimal.css")}}" rel="stylesheet">
+  <link href="{{asset("skins/minimal/minimal.css")}}" rel="stylesheet">
   <!-- =-=-=-=-=-=-= Responsive Media =-=-=-=-=-=-= -->
-
-  <link rel="stylesheet" href="{{asset(css/dropzone.css)}}">
-  <link href="{{asset(css/jquery.tagsinput.min.css)}}" rel="stylesheet">
-
-  <link href="{{asset(" css/responsive-media.css")}}" rel="stylesheet">
+  <link href="{{asset("css/responsive-media.css")}}" rel="stylesheet">
   <!-- =-=-=-=-=-=-= Template Color =-=-=-=-=-=-= -->
-  <link rel="stylesheet" id="color" href="{{asset(" css/colors/default.css")}}">
+  <link rel="stylesheet" href="{{asset("css/colors/default.css")}}">
+  <!-- JavaScripts -->
+  <script src="js/modernizr.js"></script>
+
   <script src="https://kit.fontawesome.com/a022389a13.js" crossorigin="anonymous"></script>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/js/dropify.min.js"
